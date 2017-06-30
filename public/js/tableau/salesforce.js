f1a7b3eace8974f5355b7b47001cb5440703981b
Putra Liowono <putra.liowono@bhinneka.com>
2017-06-30 13:56:32 +0700

    Login page

--- public/js/tableau/salesforce.js
+++ public/js/tableau/salesforce.js
@@ -5,7 +5,7 @@ function initEsSg(){
     var urlEsSg = "https://10az.online.tableau.com/t/bmdbi/views/ExecutiveSalesforce/SalesGrowth?:embed=y&:showAppBanner=false&:showShareOptions=true&:display_count=no&:showVizHome=no&:refresh=yes";
     var optionEsSg = {
         hideTabs: true,
-        // hideToolbar: true
+        hideToolbar: true,
         width: '100%',
         height: '1025px'
     };
